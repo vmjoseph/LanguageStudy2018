7f1c40787631d282ce883ce3bdc6cf26d5e584f9
Vallie Joseph <valliejoseph@gmail.com>
2018-01-29 11:33:54 -0500

    Adding todays notes

---
+++ notes/notes_1_29_18.hs
@@ -0,0 +1,14 @@
+{- Cons operator -- > :
+ - How lists are constructed
+ - a right associative operator
+ Characters vs Lists
+ - can access the character of a string with single quotes
+ 
+ Infix operator
+ - writing between operators
+ 
+ Function name = symbol, surrounded with back-quoted name): infix (symbolic operator)
+ Function name = name :prefix (named functions)
+-}
+putFiveOnIt = (:) 5 -- 5 is now first argument (new head) if the con function
+
