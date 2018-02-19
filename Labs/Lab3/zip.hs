eb1aa8fb9cc21896c3eaff61c9ae653e4fc5f456
Vallie Joseph <valliejoseph@gmail.com>
2018-02-19 11:49:47 -0500

    uploading zipit

--- Labs/Lab3/zip.hs
+++ Labs/Lab3/zip.hs
@@ -5,6 +5,7 @@ zippit [] _ =  ""
 splitIt :: String -> String
 splitIt = id
 
-mapIt :: String->String->[[String]]
-mapIt "" "" = [[""]]
-mapIt a b = [[[head a] ++ b]]
+mapIt :: [a]->[b]->[(a,b)]
+mapIt _ [] = []
+mapIt [] _ = []
+mapIt (x:xs) (y:ys) = (x,y) : zippit xs ys 
