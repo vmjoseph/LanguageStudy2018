ffa404fe3be36ac5aab71bccf3dca98c81c2164c
Vallie Joseph <valliejoseph@gmail.com>
2018-02-19 11:56:08 -0500

    redefining map

--- Labs/Lab3/zip.hs
+++ Labs/Lab3/zip.hs
@@ -1,11 +1,13 @@
-zippit :: [Int]-> String -> String
-zippit [a] b =  b
-zippit [] _ =  ""
-
-splitIt :: String -> String
-splitIt = id
-
 mapIt :: [a]->[b]->[(a,b)]
 mapIt _ [] = []
 mapIt [] _ = []
-mapIt (x:xs) (y:ys) = (x,y) : zippit xs ys 
+mapIt (x:xs) (y:ys) = (x,y) : mapIt xs ys 
+
+{-Cartesian product map version -}
+{-
+cartProd :: [a] -> [b] -> [(a,b)]
+cartProd [] [] = []
+cartProd (x:xs) (y:ys) = makePairs x ys
+    where makePairs ::  [a]->[b]-> (a,b)
+    makePairs x ys = map ((,)x) y s)
+-}
