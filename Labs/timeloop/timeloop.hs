c304b7cd382dc5dd50c26b717323605814cff7f5
vmjoseph <valliejoseph@gmail.com>
2018-03-19 12:20:03 -0400

    uploading lab

--- Labs/timeloop/timeloop.hs
+++ Labs/timeloop/timeloop.hs
@@ -1,12 +1,12 @@
 
 -- figure out return type
---timeloop :: Integer -> 
+-- timeloop :: Integer -> 
 
 timeloop k n = do
-    putStrLn (show n ++ " Abracadabra") -- convert value to string
-    if k < n -- function will recursivly loop until it gets to n
+    if k < n -- function will recursively loop until it gets to n
         then timeloop (k+1) -- iterates k
         else return () -- stops the loop
+    putStrLn (show n ++ " Abracadabra") -- convert value to string
 
 main :: IO ()
 main = do 
